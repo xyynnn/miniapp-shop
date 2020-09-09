b291c257df41cc90a4ce1dfa0776b7d8bd00080f
“Yxuyongna <2627269805@qq.com>
2020-09-09 19:59:30 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+//Page Object
+Page({
+  data: {
+    //轮播图数组
+    swiperList:[]
+  },
+  //页面开始加载 就会触发
+  onLoad: function(options) {
+    // 1 发送异步请求获取轮播图数据
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        console.log(result);
+      }
+    });
+      
+  }
+});
+  
