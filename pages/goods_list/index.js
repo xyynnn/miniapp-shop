66914ab41e2f95c47cd3b9c6228cd86edc79783e
xyynnn <2627269805@qq.com>
2020-09-17 18:26:58 +0800

    update

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,100 @@
-// pages/goods_list/index.js
+/*
+1 用户上滑页面 滚动条触底 开始加载下一页数据
+  1 找到滚动条触底事件 微信小程序官方文档寻找
+  2 判断还有没有下一页数据
+    1 获取到总页数 只有总条数
+      总页数 = Math.ceil(总条数/页容量 pagesize)
+      总页数    =Math.ceil( 23 / 10 ) = 3
+    2 获取到当前的页码 pagenum
+    3 判断一下 当前的页码是否大于等于 总页数
+      表示 没有下一页数据
+  3 假如没有下一页数据 弹出一个提示
+  4 假如还有下一页数据 来加载下一页数据
+    1 当前的页码 ++
+    2 重新发送请求
+    3 数据请求回来 要对data的数组 进行 拼接 而不是全部替换！！！
+*/
+import { request } from "../../request/index.js";
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: "综合",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "销售",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "价格",
+        isActive: false
+      },
+    ],
+    goodsList:[]
   },
+  //接口要的参数
+  QueryParams:{
+    query:"",
+    cid:"",
+    pagenum:1,
+    pagesize:10
+  },
+  //总页数
+  totalPages:1,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // console.log(options)
+    this.QueryParams.cid=options.cid;
+    this.getGoodsList();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取商品列表数据
+  async getGoodsList(){
+    const res=await request({url:"/goods/search",data:this.QueryParams});
+    // console.log(res);
+    //获取总条数
+    const total=res.total;
+    //计算总页数
+    this.totalPages=Math.ceil(total/this.QueryParams.pagesize)
+    this.setData({
+      //拼接了数组
+      goodsList:[...this.data.goodslist,...res.goods]
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //标题点击事件 从子组件传递过来
+  handleTabsItemChange(e){
+    // console.log(e);
+    //1 获取被点击的标题索引
+    const {index}=e.detail;
+    //2 修改源数组
+    let {tabs}=this.data;
+    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+    //3 赋值到data中
+    this.setData({
+      tabs
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //页面上滑  滚动条触底事件
+  onReachBottom(){
+    //1 判断还有没有下一页数据
+    if(this.QueryParams.pagenum>=this.totalPages){
+      //没有下一页数据
+      console.log('%c'+"没有下一页数据","color:red;font-size:100px;background-image:linear-gradient(to right,#0094ff,pink)")
+    }else{
+      //还有下一页数据
+      // console.log('%c'+"有下一页数据","color:red;font-size:100px;background-image:linear-gradient(to right,#0094ff,pink)")
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -22,31 +22,32 @@ Page({
     // });
     this.getSwiperList();
     this.getCateList();
+    this.getFloorList();
   },
 
   //获取轮播图数据 
   getSwiperList() {
-    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata' }).then(result => {
+    request({ url: '/home/swiperdata' }).then(result => {
       this.setData({
-        swiperList: result.data.message
+        swiperList: result
       })
     })
   },
 
   //获取分类导航数据 
   getCateList() {
-    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems' }).then(result => {
+    request({ url: '/home/catitems' }).then(result => {
       this.setData({
-        cateList: result.data.message
+        cateList: result
       })
     })
   },
 
   //获取楼层导航数据 
   getFloorList() {
-    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata' }).then(result => {
+    request({ url: '/home/floordata' }).then(result => {
       this.setData({
-        floorList: result.data.message
+        floorList: result
       })
     })
   }
